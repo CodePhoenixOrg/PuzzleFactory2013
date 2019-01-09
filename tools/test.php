55220ba4508b3264f486661bde2cbfb305d2bc84
David BLANCHARD <david@oiseau.home>
2019-01-09 08:35:13 +0100

    Add regex test

---
+++ tools/test.php
@@ -0,0 +1,9 @@
+<?php
+
+	$re = '/(([a-z]*)\()?(\\\\([0-9]))(\))?/m';
+	$str = 'strtolower(\\1).strtoupper(\\2)';
+	$subst = '\\1$match[\\4]\\5';
+	
+	$result = preg_replace($re, $subst, $str);
+	
+	echo "The result of the substitution is ".$result . PHP_EOL;
